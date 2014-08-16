2f2e4e3bfd377bcf45f93d15c67a4a8d05543b35
Ashley Davis (SgtCoDFish) <ashley_davis10419@hotmail.com>
2014-08-16 19:25:41 +0100

    Add skeleton of IteratingSystem test

---
+++ test/systems/IteratingSystemTest.cpp
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright 2014 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+#include "Ashley/core/Component.hpp"
+#include "Ashley/systems/IteratingSystem.hpp"
+
+#include "gtest/gtest.h"
+
+using ashley::test::PositionComponent;
+using ashley::test::VelocityComponent;
+
+namespace {
+class IteratingSystemTest : public ::testing::Test {
+protected:
+
+};
+
+class IteratingSystemMock : public ashley::IteratingSystem {
+public:
+	int numUpdates;
+
+	IteratingSystemMock(ashley::Family &family) : numUpdates(0), IteratingSystem(family) {}
+	virtual ~IteratingSystemMock() {}
+
+	void processEntity(ashley::Entity &entity, float deltaTime) override {
+		++numUpdates;
+	}
+};
+
+class ComponantA : public ashley::Component {
+
+};
+
+class ComponantB : public ashley::Component {
+
+};
+
+class ComponantC : public ashley::Component {
+
+};
+}
+
+TEST_F(IteratingSystemTest, ShouldIterateEntitiesWithCorrectFamily) {
+
+}
