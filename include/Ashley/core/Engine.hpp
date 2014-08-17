e680434de58215a73b50ee206723def248599759
Ashley Davis (SgtCoDFish) <ashley_davis10419@hotmail.com>
2014-08-17 23:25:47 +0100

    Begin implementing Engine; not yet complete.

--- include/Ashley/core/Engine.hpp
+++ include/Ashley/core/Engine.hpp
@@ -17,8 +17,11 @@
 #ifndef ENGINE_HPP_
 #define ENGINE_HPP_
 
+#include <typeinfo>
 #include <typeindex>
 #include <vector>
+#include <unordered_map>
+#include <memory>
 
 namespace ashley {
 class Family;
@@ -49,14 +52,34 @@ public:
 	Engine();
 
 	/**
-	 * Adds an entity to this Engine.
+	 * <p>Adds an {@link Entity} to this {@link Engine}.</p>
+	 *
+	 * <p>Note that once added, ownership is transferred to the Engine. For access to the {@link Entity} after adding,
+	 * use {@link Engine#addEntityAndGet} or use the overload which takes an std::shared_ptr.</p>
 	 */
 	void addEntity(ashley::Entity &entity);
 
 	/**
-	 * Removes an entity from this Engine.
+	 * <p>Adds an std::shared_ptr to an {@link Entity} to this {@link Engine}.</p>
+	 */
+	void addEntity(std::shared_ptr<ashley::Entity> ptr);
+
+	/**
+	 * <p>Adds an {@link Entity} to this Engine and returns an std::shared_ptr to it.</p>
+	 */
+	std::shared_ptr<ashley::Entity> addEntityAndGet(ashley::Entity &entity);
+
+	/**
+	 * <p>Removes an {@link Entity} from this {@link Engine}.</p>
+	 *
+	 * <p>Note that if no external pointers have been maintained, the {@link Entity} will be destroyed immediately.</p>
 	 */
 	void removeEntity(ashley::Entity &entity);
+
+	/**
+	 * Removes an {@link Entity} from this {@link Engine} and returns it in a shared_ptr.
+	 */
+	std::shared_ptr<ashley::Entity> removeEntityAndGet(ashley::Entity &entity);
 
 	/**
 	 * Removes all entities registered with this Engine.
@@ -65,31 +88,54 @@ public:
 
 	/**
 	 * Adds the {@link EntitySystem} to this Engine.
+	 *
+	 * <p>Note that once added, ownership is transferred to the Engine. For access to the {@link EntitySystem} after
+	 * adding, use {@link Engine#addSystemAndGet}.</p>
 	 */
 	void addSystem(ashley::EntitySystem &system);
 
 	/**
-	 * Removes the {@link EntitySystem} from this Engine.
+	 * Adds the {@link EntitySystem} to this Engine via an std::shared_ptr.
+	 *
+	 * <p>Note that once added, ownership is transferred to the Engine. For access to the {@link EntitySystem} after
+	 * adding, use {@link Engine#addSystemAndGet}.</p>
+	 */
+	void addSystem(std::shared_ptr<ashley::EntitySystem> system);
+
+	/**
+	 * <p>Adds an {@link EntitySystem} to this Engine and returns an std::shared_ptr to it.</p>
+	 */
+	std::shared_ptr<ashley::EntitySystem> addSystemAndGet(ashley::EntitySystem &system);
+
+	/**
+	 * <p>Removes the {@link EntitySystem} from this Engine.</p>
+	 * <p>Note that if no external pointers have been maintained, the {@link EntitySystem} will be destroyed
+	 * immediately. To preserve it, use {@link Engine#removeSystemAndGet}.</p>
 	 */
 	void removeSystem(ashley::EntitySystem &system);
 
 	/**
-	 * Quick {@link EntitySystem} retrieval.
+	 * Removes an {@link EntitySystem} from this {@link Engine} and returns it in a shared_ptr.
 	 */
-	EntitySystem& getSystem(std::type_index &systemType) const;
+	std::shared_ptr<ashley::EntitySystem> removeSystemAndGet(ashley::EntitySystem &system);
 
 	/**
-	 * Returns const vector of entities for the specified {@link Family}.
+	 * <p>Quick {@link EntitySystem} retrieval.</p>
 	 */
-	const std::vector<ashley::Entity *> getEntitiesFor(ashley::Family &family);
+	std::weak_ptr<ashley::EntitySystem> getSystem(std::type_index &systemType) const;
 
 	/**
-	 * Adds an {@link EntityListener}
+	 * <p>Returns const vector of {@link Entity}s for the specified {@link Family}.</p>
+	 */
+	const std::vector<std::shared_ptr<ashley::Entity>> getEntitiesFor(ashley::Family &family) const;
+
+	/**
+	 * Adds an {@link EntityListener}.
 	 */
 	void addEntityListener(ashley::EntityListener &listener);
 
 	/**
-	 * Removes an {@link EntityListener}
+	 * Removes an {@link EntityListener}.
 	 */
 	void removeEntityListener(ashley::EntityListener &listener);
 
@@ -98,6 +144,39 @@ public:
 	 * @param deltaTime The time passed since the last frame.
 	 */
 	void update(float deltaTime);
+
+private:
+	std::vector<std::shared_ptr<ashley::Entity>> entities;
+
+	std::vector<std::shared_ptr<ashley::EntitySystem>> systems;
+	std::unordered_map<std::type_index, std::vector<std::shared_ptr<ashley::EntitySystem>>>systemsByClass;
+
+	std::unordered_map<ashley::Family, std::vector<std::shared_ptr<ashley::Entity>>> families;
+	std::unordered_map<ashley::Family, const std::vector<std::shared_ptr<ashley::Entity>>> immutableFamilies;
+
+	std::vector<ashley::EntityListener *> listeners;
+	std::vector<ashley::EntityListener *> removalPendingListeners;
+
+	bool notifying;
+
+	ashley::Listener<ashley::Entity> *componentAddedListener;
+	ashley::Listener<ashley::Entity> *componentRemovedListener;
+
+	void componentAdded(const ashley::Entity &entity);
+	void componentRemoved(const ashley::Entity &entity);
+	void removePendingListeners();
+
+//	class AddedListener : public ashley::Listener<Entity> {
+//		virtual void receieve(const ashley::Signal<ashley::Entity> &signal, const ashley::Entity &object) override {
+//			componentAdded(object);
+//		}
+//	};
+//
+//	class RemovedListener : public ashley::Listener<Entity> {
+//		virtual void receieve(const ashley::Signal<ashley::Entity> &signal, const ashley::Entity &object) override {
+//			componentRemoved(object);
+//		}
+//	};
 };
 }
 
--- src/Engine.cpp
+++ src/Engine.cpp
@@ -14,11 +14,175 @@
  * limitations under the License.
  ******************************************************************************/
 
+#include <cassert>
+
 #include <vector>
+#include <unordered_map>
+#include <memory>
+#include <algorithm>
 
 #include "Ashley/core/Family.hpp"
 #include "Ashley/core/Engine.hpp"
+#include "Ashley/core/Entity.hpp"
+#include "Ashley/core/EntityListener.hpp"
+#include "Ashley/core/EntitySystem.hpp"
 
-const std::vector<ashley::Entity *> ashley::Engine::getEntitiesFor(ashley::Family &family) {
+ashley::Engine::Engine() :
+		componentAddedListener(), componentRemovedListener(), notifying(false) {
+
+}
+
+void ashley::Engine::addEntity(std::shared_ptr<ashley::Entity> ptr) {
+	entities.emplace_back(ptr);
+
+	for (auto &p : families) {
+		const Family& family = p.first;
+		std::vector<std::shared_ptr<ashley::Entity>>& vec = p.second;
+
+		if (family.matches(*ptr)) {
+			vec.emplace_back(std::shared_ptr<ashley::Entity>(ptr));
+			ptr->getFamilyBits().set(family.getIndex(), true);
+		}
+	}
+
+	ptr->componentAdded.add(componentAddedListener);
+	ptr->componentRemoved.add(componentRemovedListener);
+
+	notifying = true;
+
+	for (auto &listener : listeners) {
+		listener->entityAdded(*ptr);
+	}
+
+	notifying = false;
+	removePendingListeners();
+}
+
+void ashley::Engine::addEntity(ashley::Entity &entity) {
+	addEntity(std::make_shared<ashley::Entity>(entity));
+}
+
+std::shared_ptr<ashley::Entity> ashley::Engine::addEntityAndGet(ashley::Entity &entity) {
+	auto ptr = std::make_shared<ashley::Entity>(entity);
+	addEntity(ptr);
+
+	return std::shared_ptr<ashley::Entity>(ptr);
+}
+
+void ashley::Engine::removeEntity(ashley::Entity &entity) {
+	// saves implementing twice
+	removeEntityAndGet(entity);
+}
+
+std::shared_ptr<ashley::Entity> ashley::Engine::removeEntityAndGet(ashley::Entity &entity) {
+	auto entitiesIt = std::find_if(entities.begin(), entities.end(),
+			[](std::shared_ptr<ashley::Entity> ptr) {return *ptr == entity;});
+
+	if(entitiesIt == entities.end()) {
+		return std::shared_ptr(nullptr);
+	}
+
+	auto ret = *entitiesIt;
+
+	entities.erase(entitiesIt);
+
+	if(!ret->getFamilyBits().none()) {
+		for (auto &p : families) {
+			const Family& family = p.first;
+			std::vector<std::shared_ptr<ashley::Entity>>& vec = p.second;
+
+			if(family.matches(entity)) {
+				auto vecIt = std::find_if(vec.begin(), vec.end(), [](std::shared_ptr<ashley::Entity> ptr){return *ptr == entity;});
+
+				if(vecIt != vec.end()) {
+					vec.erase(vecIt);
+				}
+
+				ret->getFamilyBits().set(family.getIndex(), false);
+			}
+		}
+	}
+
+	ret->componentAdded.remove(componentAddedListener);
+	ret->componentRemoved.remove(componentRemovedListener);
+
+	notifying = true;
+
+	for(auto &listener : listeners) {
+		listener->entityRemoved(*ret);
+	}
+
+	notifying = false;
+	removePendingListeners();
+}
+
+void ashley::Engine::removeAllEntities() {
+	while (entities.size() > 0) {
+		removeEntity(*entities.front());
+	}
+}
+
+void ashley::Engine::addSystem(std::shared_ptr<ashley::EntitySystem> system) {
+
+}
+
+void ashley::Engine::addSystem(ashley::EntitySystem &system) {
+
+}
+
+std::shared_ptr<ashley::EntitySystem> ashley::Engine::addSystemAndGet(
+		ashley::EntitySystem &system) {
+
+}
+
+void ashley::Engine::removeSystem(ashley::EntitySystem &system) {
+
+}
+
+std::shared_ptr<ashley::EntitySystem> ashley::Engine::removeSystemAndGet(
+		ashley::EntitySystem &system) {
+
+}
+
+std::weak_ptr<ashley::EntitySystem> getSystem(std::type_index &systemType) const {
+
+}
+
+const std::vector<std::shared_ptr<ashley::Entity>> ashley::Engine::getEntitiesFor(
+		ashley::Family &family) const {
 	return std::vector<ashley::Entity *>();
+}
+
+void ashley::Engine::addEntityListener(ashley::EntityListener &listener) {
+
+}
+
+void ashley::Engine::removeEntityListener(ashley::EntityListener &listener) {
+
+}
+
+void ashley::Engine::update(float deltaTime) {
+
+}
+
+void ashley::Engine::componentAdded(const ashley::Entity &entity) {
+
+}
+
+void ashley::Engine::componentRemoved(const ashley::Entity &entity) {
+
+}
+
+void ashley::Engine::removePendingListeners() {
+	// read as:
+	// for each element in listeners, remove if the same element is found somewhere in removalPendingListeners
+	auto it =
+			std::remove_if(listeners.begin(), listeners.end(),
+					[&](ashley::EntityListener *listenerEntry) {
+						return std::find_if(removalPendingListeners.begin(), removalPendingListeners.end(),
+								[&](ashley::EntityListener *l) {return listenerEntry == l;}) != removalPendingListeners.end();
+					});
+	listeners.erase(it, listeners.end());
+
+	removalPendingListeners.clear();
 }
