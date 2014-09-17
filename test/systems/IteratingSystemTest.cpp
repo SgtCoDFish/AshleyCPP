25b6548d83728c193b2b0df0ef39df4b8e6cf002
Ashley Davis (SgtCoDFish) <ashley_davis10419@hotmail.com>
2014-09-17 13:22:54 +0100

    Add test for Java IteratingSystem entity removal issue during iteration

--- test/systems/IteratingSystemTest.cpp
+++ test/systems/IteratingSystemTest.cpp
@@ -22,6 +22,7 @@
 #include "Ashley/core/Engine.hpp"
 #include "Ashley/core/Family.hpp"
 #include "Ashley/core/Entity.hpp"
+#include "Ashley/core/ComponentMapper.hpp"
 #include "Ashley/systems/IteratingSystem.hpp"
 #include "AshleyTestCommon.hpp"
 
@@ -34,6 +35,7 @@ using ashley::Entity;
 using ashley::Engine;
 using ashley::Family;
 using ashley::Component;
+using ashley::ComponentMapper;
 using ashley::IteratingSystem;
 
 class ComponentA : public Component {
@@ -51,6 +53,8 @@ class ComponentC : public Component {
 namespace {
 class IteratingSystemTest : public ::testing::Test {
 protected:
+	constexpr static float delta = 0.15f;
+
 	Engine engine;
 
 	std::shared_ptr<Family> family;
@@ -77,11 +81,84 @@ public:
 	}
 };
 
+class SpyComponent : public ashley::Component {
+public:
+	int64_t updates = 0;
+};
+
+class IndexComponent : public ashley::Component {
+public:
+	IndexComponent(int nindex = 0) :
+			index(nindex) {
+	}
+
+	int64_t index = 0;
+};
+
+class IteratingComponentRemovalMock : public IteratingSystem {
+private:
+	ComponentMapper<SpyComponent> sm;
+	ComponentMapper<IndexComponent> im;
+
+public:
+	IteratingComponentRemovalMock(int priority = 0) :
+			IteratingSystem(
+					ashley::Family::getFor( { typeid(SpyComponent), typeid(IndexComponent) }),
+					priority), sm(ComponentMapper<SpyComponent>::getMapper()), im(
+					ComponentMapper<IndexComponent>::getMapper()) {
+	}
+
+	void processEntity(std::shared_ptr<Entity> &entity, float deltaTime) override {
+		auto index = im.get(entity)->index;
+
+		if (index % 2 == 0) {
+			entity->remove<SpyComponent>();
+			entity->remove<IndexComponent>();
+		} else {
+			sm.get(entity)->updates++;
+		}
+	}
+};
+
+class IteratingRemovalMock : public IteratingSystem {
+private:
+	Engine *engine = nullptr;
+
+	ComponentMapper<SpyComponent> sm;
+	ComponentMapper<IndexComponent> im;
+
+public:
+	IteratingRemovalMock(int priority = 0) :
+			IteratingSystem(
+					ashley::Family::getFor( { typeid(SpyComponent), typeid(IndexComponent) }),
+					priority), sm(ComponentMapper<SpyComponent>::getMapper()), im(
+					ComponentMapper<IndexComponent>::getMapper()) {
+	}
+
+	void addedToEngine(Engine &engine) override {
+		IteratingSystem::addedToEngine(engine);
+		this->engine = &engine;
+	}
+
+	void removedFromEngine(Engine &engine) override {
+		IteratingSystem::removedFromEngine(engine);
+		this->engine = nullptr;
+	}
+
+	void processEntity(std::shared_ptr<Entity> &entity, float deltaTime) override {
+		auto index = im.get(entity)->index;
+
+		if (index % 2 == 0) {
+			engine->removeEntity(entity);
+		} else {
+			sm.get(entity)->updates++;
+		}
+	}
+};
+
 }
 
 TEST_F(IteratingSystemTest, ShouldIterateEntitiesWithCorrectFamily) {
-	const float delta = 0.15f;
-
 	auto mockSystem = std::make_shared<IteratingSystemMock>(family);
 
 	engine.addSystem(mockSystem);
@@ -107,4 +184,66 @@ TEST_F(IteratingSystemTest, ShouldIterateEntitiesWithCorrectFamily) {
 	engine.update(delta);
 
 	ASSERT_EQ(mockSystem->numUpdates, 2);
+}
+
+TEST_F(IteratingSystemTest, EntityRemovalWhileIterating) {
+	// only uses the engine from IteratingSystemTest
+
+	auto entities = engine.getEntitiesFor(Family::getFor( { typeid(SpyComponent),
+			typeid(IndexComponent) }));
+	auto sm = ComponentMapper<SpyComponent>::getMapper();
+
+	engine.addSystem(std::make_shared<IteratingRemovalMock>(10));
+
+	constexpr int numEntities = 10;
+
+	for (int i = 0; i < numEntities; i++) {
+		auto e = std::make_shared<Entity>();
+
+		e->add<SpyComponent>();
+		e->add<IndexComponent>(i + 1);
+
+		engine.addEntity(e);
+	}
+
+	engine.update(delta);
+
+	ASSERT_EQ(numEntities / 2, entities->size());
+
+	for (unsigned int i = 0; i < entities->size(); i++) {
+		auto e = entities->at(i);
+
+		ASSERT_EQ(1, sm.get(e)->updates);
+	}
+}
+
+TEST_F(IteratingSystemTest, ComponentRemovalWhileIterating) {
+	// only uses the engine from IteratingSystemTest
+
+	auto entities = engine.getEntitiesFor(Family::getFor( { typeid(SpyComponent),
+			typeid(IndexComponent) }));
+	auto sm = ComponentMapper<SpyComponent>::getMapper();
+
+	engine.addSystem(std::make_shared<IteratingComponentRemovalMock>(10));
+
+	constexpr int numEntities = 10;
+
+	for (int i = 0; i < numEntities; i++) {
+		auto e = std::make_shared<Entity>();
+
+		e->add<SpyComponent>();
+		e->add<IndexComponent>(i+1);
+
+		engine.addEntity(e);
+	}
+
+	engine.update(delta);
+
+	ASSERT_EQ(numEntities / 2, entities->size());
+
+	for(unsigned int i = 0; i < entities->size(); i++) {
+		auto e = entities->at(i);
+
+		ASSERT_EQ(1, sm.get(e)->updates);
+	}
 }
