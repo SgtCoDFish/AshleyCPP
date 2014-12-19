764991318ce0a2b97a007737225c887cbcfa2d51
Ashley Davis (SgtCoDFish) <ashley_davis10419@hotmail.com>
2014-12-19 19:18:01 +0000

    Re-enable commented out signal test.

--- test/signals/SignalTests.cpp
+++ test/signals/SignalTests.cpp
@@ -49,7 +49,7 @@ class SignalTest : public ::testing::Test {
 protected:
 	Dummy dummy;
 	Signal<Dummy> signal;
-	std::unique_ptr<Listener<Dummy>> listener;
+	std::unique_ptr<ListenerMock> listener;
 
 	SignalTest() :
 			dummy(), signal(), listener{new ListenerMock()} {
@@ -101,35 +101,33 @@ TEST_F(SignalTest, AddListenersAndDispatch) {
 	}
 }
 
-//// Check that removing a listener works correctly.
-//TEST_F(SignalTest, AddListenerDispatchAndRemove) {
-//	std::shared_ptr<ListenerMock> lBPtr = std::make_shared<ListenerMock>();
-//	auto base = std::dynamic_pointer_cast<Listener<Dummy>>(lBPtr);
-//	auto dynList = (ListenerMock *)(listener.get());
-//	signal.add(listener);
-//	signal.add(base);
-//
-//	const int numDispatches = 5;
-//
-//	for (int i = 0; i < numDispatches; i++) {
-//		ASSERT_EQ(i, dynList->count);
-//		ASSERT_EQ(i, lBPtr->count);
-//
-//		signal.dispatch(dummy);
-//
-//		ASSERT_EQ(i + 1, dynList->count);
-//		ASSERT_EQ(i + 1, lBPtr->count);
-//	}
-//
-//	signal.remove(base);
-//
-//	for (int i = 0; i < numDispatches; i++) {
-//		ASSERT_EQ(i + numDispatches, dynList->count);
-//		ASSERT_EQ(numDispatches, lBPtr->count);
-//
-//		signal.dispatch(dummy);
-//
-//		ASSERT_EQ(i + 1 + numDispatches, dynList->count);
-//		ASSERT_EQ(numDispatches, lBPtr->count)<< "Listener not removed correctly.";
-//	}
-//}
+// Check that removing a listener works correctly.
+TEST_F(SignalTest, AddListenerDispatchAndRemove) {
+	auto base = std::unique_ptr<ListenerMock>(new ListenerMock());
+	signal.add(listener.get());
+	signal.add(base.get());
+
+	const int numDispatches = 5;
+
+	for (int i = 0; i < numDispatches; i++) {
+		ASSERT_EQ(i, base->count);
+		ASSERT_EQ(i, listener->count);
+
+		signal.dispatch(&dummy);
+
+		ASSERT_EQ(i + 1, base->count);
+		ASSERT_EQ(i + 1, listener->count);
+	}
+
+	signal.remove(base.get());
+
+	for (int i = 0; i < numDispatches; i++) {
+		ASSERT_EQ(i + numDispatches, listener->count);
+		ASSERT_EQ(numDispatches, base->count);
+
+		signal.dispatch(&dummy);
+
+		ASSERT_EQ(i + 1 + numDispatches, listener->count);
+		ASSERT_EQ(numDispatches, base->count)<< "Listener not removed correctly.";
+	}
+}
